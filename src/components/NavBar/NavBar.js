735a249cf007caa70ca47e1713126fbac7929043
Bryant <cabrera.bryant@gmail.com>
2019-05-08 21:27:24 -0700

    Bryant adds contract and expand functionality on hover to NavBar.js.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,27 +5,30 @@ import './NavBar.css';
 
 class NavBar extends Component {
     state = {
-        navLinearDisplay: 'flex',
-        navHamburgerDisplay: 'none'
+        logoContractedDisplay: "",
+        logoExpandedDisplay: "none",
+        textDisplay: "none"
     }
 
-    componentDidMount = () => {
-        window.addEventListener('scroll', this.listenScrollEvent)
+    expand = () => {
+        console.log('expanding');
+        this.setState({
+            logoContractedDisplay: "none",
+            logoExpandedDisplay: "",
+            textDisplay: ""
+        });
     }
 
-    listenScrollEvent = e => {
-        if (window.scrollY > 400) {
-            this.setState({
-                navLinearDisplay: 'none',
-                navHamburgerDisplay: 'block'
-            });
-        } else {
-            this.setState({
-                navLinearDisplay: 'flex',
-                navHamburgerDisplay: 'none'
-            });
-        }
+    contract = () => {
+        console.log('contracting');
+
+        this.setState({
+            logoContractedDisplay: "",
+            logoExpandedDisplay: "none",
+            textDisplay: "none"
+        });
     }
+
 
     render() {
         NavItem.propTypes = {
@@ -34,80 +37,108 @@ class NavBar extends Component {
         }
 
         return (
-            <div className="navbar">
+            <div className="navbar"
+                onMouseEnter={this.expand}
+                onMouseLeave={this.contract}
+            >
                 <div className="navbar__logo">
                     <RouterLink exact to="/" title="Landing Page">
                         <img
                             className="navbar__logo__img"
-                            src="/imgs/nav/nationbuilder-horizontal-black.png"
+                            src="/imgs/nav/nationbuilder-logo-contracted.png"
                             alt="NationBuilder Logo"
-                            title="NationBuilder Logo">
+                            title="NationBuilder Logo"
+                            style={{ display: this.state.logoContractedDisplay }}
+                        >
+                        </img>
+                        <img
+                            className="navbar__logo__img"
+                            src="/imgs/nav/nationbuilder-logo-expanded.png"
+                            alt="NationBuilder Logo"
+                            title="NationBuilder Logo"
+                            style={{ display: this.state.logoExpandedDisplay }}
+                        >
                         </img>
                     </RouterLink>
                 </div>
                 <ul className="navbar__list nav-stacked flex-column">
                     <li className="navbar__list__item">
-                        <img
+                        <RouterLink exact to="#dashboard" title="Dashboard" className="navbar__list__item__link">
+                            <img
                             className="navbar__list__item__icon"
                             src="/imgs/nav/dashboard-icon.png"
                             alt="Dashboard Icon"
                             title="Dashboard Icon">
-                        </img>
-                        <RouterLink exact to="#dashboard" title="Dashboard" className="navbar__list__item__link">Dashboard</RouterLink>
+                            </img>
+                            <span style={{display: this.state.textDisplay}}>Dashboard</span>
+                        </RouterLink>
                     </li>
                     <li className="navbar__list__item">
-                        <img
-                            className="navbar__list__item__icon"
-                            src="/imgs/nav/people-icon.png"
-                            alt="People Icon"
-                            title="People Icon">
-                        </img>
-                        <RouterLink exact to="#people" title="People Page" className="navbar__list__item__link">People</RouterLink>
+                        
+                        <RouterLink exact to="#people" title="People Page" className="navbar__list__item__link">
+                            <img
+                                className="navbar__list__item__icon"
+                                src="/imgs/nav/people-icon.png"
+                                alt="People Icon"
+                                title="People Icon">
+                            </img>
+                            <span style={{display: this.state.textDisplay}}>People</span>
+                        </RouterLink>
                     </li>
                     <li className="navbar__list__item">
-                        <img
-                            className="navbar__list__item__icon"
-                            src="/imgs/nav/website-icon.png"
-                            alt="Website Icon"
-                            title="Website Icon">
-                        </img>
-                        <RouterLink exact to="#website" title="Website Page" className="navbar__list__item__link">Website</RouterLink>
+                        <RouterLink exact to="#website" title="Website Page" className="navbar__list__item__link">
+                            <img
+                                className="navbar__list__item__icon"
+                                src="/imgs/nav/website-icon.png"
+                                alt="Website Icon"
+                                title="Website Icon">
+                            </img>
+                            <span style={{display: this.state.textDisplay}}>Website</span>
+                        </RouterLink>
                     </li>
                     <li className="navbar__list__item">
-                        <img
-                            className="navbar__list__item__icon"
-                            src="/imgs/nav/communication-icon.png"
-                            alt="Communication Icon"
-                            title="Communication Icon">
-                        </img>
-                        <RouterLink exact to="#communication" title="Communication Page" className="navbar__list__item__link">Communication</RouterLink>
+                        <RouterLink exact to="#communication" title="Communication Page" className="navbar__list__item__link">
+                            <img
+                                className="navbar__list__item__icon"
+                                src="/imgs/nav/communication-icon.png"
+                                alt="Communication Icon"
+                                title="Communication Icon">
+                            </img>
+                            <span style={{display: this.state.textDisplay}}>Communication</span>
+                        </RouterLink>
                     </li>
                     <li className="navbar__list__item">
-                        <img
-                            className="navbar__list__item__icon"
-                            src="/imgs/nav/finances-icon.png"
-                            alt="Finances Icon"
-                            title="Finances Icon">
-                        </img>
-                        <RouterLink exact to="#finances" title="Finances Page" className="navbar__list__item__link">Finances</RouterLink>
+                        <RouterLink exact to="#finances" title="Finances Page" className="navbar__list__item__link">
+                            <img
+                                className="navbar__list__item__icon"
+                                src="/imgs/nav/finances-icon.png"
+                                alt="Finances Icon"
+                                title="Finances Icon">
+                            </img>
+                            <span style={{display: this.state.textDisplay}}>Finances</span>
+                        </RouterLink>
                     </li>
                     <li className="navbar__list__item">
-                    <img
-                            className="navbar__list__item__icon"
-                            src="/imgs/nav/nations-icon.png"
-                            alt="Nations Icon"
-                            title="Nations Icon">
-                        </img>
-                        <RouterLink exact to="#nations" title="Nations Page" className="navbar__list__item__link">Nations</RouterLink>
+                        <RouterLink exact to="#nations" title="Nations Page" className="navbar__list__item__link">
+                            <img
+                                className="navbar__list__item__icon"
+                                src="/imgs/nav/nations-icon.png"
+                                alt="Nations Icon"
+                                title="Nations Icon">
+                            </img>
+                            <span style={{display: this.state.textDisplay}}>Nations</span>
+                        </RouterLink>
                     </li>
                     <li className="navbar__list__item">
-                    <img
-                            className="navbar__list__item__icon"
-                            src="/imgs/nav/settings-icon.png"
-                            alt="Settings Icon"
-                            title="Settings Icon">
-                        </img>
-                        <RouterLink exact to="#settings" title="Settings" className="navbar__list__item__link">Settings</RouterLink>
+                        <RouterLink exact to="#settings" title="Settings" className="navbar__list__item__link">
+                            <img
+                                className="navbar__list__item__icon"
+                                src="/imgs/nav/settings-icon.png"
+                                alt="Settings Icon"
+                                title="Settings Icon">
+                            </img>
+                            <span style={{display: this.state.textDisplay}}>Settings</span>
+                        </RouterLink>
                     </li>
                 </ul>
                 {/* <ul className="nav flex-column">
