d84fe4de54b6efea370515080850d19fdad716c9
Bryant <cabrera.bryant@gmail.com>
2019-05-08 20:07:40 -0700

    Bryant adds CSS reset to App.css.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+import { Nav, NavItem } from 'reactstrap';
 import './NavBar.css';
 
 class NavBar extends Component {
@@ -27,6 +28,11 @@ class NavBar extends Component {
     }
 
     render() {
+        NavItem.propTypes = {
+            // pass in custom element to use
+            color: "white"
+        }
+
         return (
             <div className="navbar">
                 {/* <div className="navbar__logo">
@@ -34,15 +40,62 @@ class NavBar extends Component {
                         <img className="navbar__logo__img" src="/imgs/logo.png" alt="NationBuilder Logo" title="NationBuilder Logo"></img>
                     </NavLink>
                 </div> */}
-                <nav className="navbar__nav--linear">
+                {/* <nav className="navbar__nav--linear">
                     <NavLink exact to="#dashboard" title="Dashboard">Dashboard</NavLink>
                     <NavLink exact to="#people" title="People Page">People</NavLink>
                     <NavLink exact to="#website" title="Website Page">Website</NavLink>
                     <NavLink exact to="#communication" title="Communication Page">Communication</NavLink>
                     <NavLink exact to="#finances" title="Finances Page">Finances</NavLink>
-                    <NavLink exact to="#cnations" title="Nations Page">Nations</NavLink>
+                    <NavLink exact to="#nations" title="Nations Page">Nations</NavLink>
                     <NavLink exact to="#settings" title="Settings">Settings</NavLink>
-                </nav>
+                </nav> */}
+                {/* <ul className="nav flex-column">
+                    <li className="nav-item">
+                        <NavLink exact to="#dashboard" title="Dashboard">Dashboard</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to="#people" title="People Page">People</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to="#website" title="Website Page">Website</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to="#communication" title="Communication Page">Communication</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to="#finances" title="Finances Page">Finances</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to="#nations" title="Nations Page">Nations</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink exact to="#settings" title="Settings">Settings</NavLink>
+                    </li>
+                </ul> */}
+                <Nav vertical className="nav" style={{ height: 1010, width: 253, backgroundColor: "#000" }}>
+                    <img src="/imgs/nationbuilder-horizontal-black.png" className="nav__logo img-responsive" alt="NationBuilder Logo" title=" NationBuilder Logo"></img>
+                    <NavItem>
+                        <NavLink exact to="#dashboard" title="Dashboard" className="text-white">Dashboard</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="#people" title="People Page">People</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="#website" title="Website Page">Website</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="#communication" title="Communication Page">Communication</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="#finances" title="Finances Page">Finances</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="#nations" title="Nations Page">Nations</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="#settings" title="Settings">Settings</NavLink>
+                    </NavItem>
+                </Nav>
             </div>
         )
     }
